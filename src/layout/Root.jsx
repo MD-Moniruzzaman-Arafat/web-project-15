9c9c32d37ba2a482e2720a97af57cef6069a8ac9
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-08-07 23:37:08 +0600

    navbar component design

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router";
 import Title from "../components/header/Title";
 import MarqueeText from "../components/marqueeText/MarqueeText";
+import Navbar from "../components/navbar/Navbar";
 
 const Root = () => {
   return (
     <>
       <Title />
       <MarqueeText />
+      <Navbar />
       <Outlet />
     </>
   );

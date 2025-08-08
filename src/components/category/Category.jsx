9390746a649c6d38dfb673bc6f39fb27025be2c7
MD-Moniruzzaman-Arafat <arafatmonir091@gmail.com>
2025-08-08 21:22:37 +0600

    category component design

---
+++ src/components/category/Category.jsx
@@ -0,0 +1,38 @@
+const Category = () => {
+  return (
+    <>
+      <ul className="menu bg-base-200 rounded-box w-full">
+        <li className="menu-title">All Category</li>
+        <li>
+          <a>National News</a>
+        </li>
+        <li>
+          <a>Breaking News</a>
+        </li>
+        <li>
+          <a>Regular News</a>
+        </li>
+        <li>
+          <a>International News</a>
+        </li>
+        <li>
+          <a>Sports</a>
+        </li>
+        <li>
+          <a>Entertainment</a>
+        </li>
+        <li>
+          <a>Culture</a>
+        </li>
+        <li>
+          <a>Arts</a>
+        </li>
+        <li>
+          <a>All News</a>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Category;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
+import Category from "../components/category/Category";
+
 const HomePage = () => {
   return (
     <>
-      <h1>home</h1>
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-4 gap-2">
+        <div className="lg:col-span-1 px-5">
+          <Category />
+        </div>
+        <div className="lg:col-span-2 bg-amber-950">a</div>
+        <div className="lg:col-span-1 bg-blue-600">d</div>
+      </div>
     </>
   );
 };
